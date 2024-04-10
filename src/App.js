e5a669e03f24ac9303a3e30e0db52c2e6ee90a4e
Mohit <mohit132m@gmail.com>
2024-04-10 12:26:40 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Wheel } from 'react-custom-roulette'
+import "./App.css"
 
-function App() {
+const data = [
+  { option: 'Yellow' },
+  { option: 'Green' },
+  { option: 'Red' },
+  { option: 'Blue' },
+  { option: 'Purple' },
+  { option: 'Orange' },
+  { option: 'White' },
+  { option: 'Brown' },
+  { option: 'Pink' }
+]
+const logic = 
+ { Red: 'Passion, excitement, love',
+Pink: 'Soft, reserved, earthy',
+Purple: 'Mysterious, noble, glamorous',
+Blue: 'Wisdom, hope, reason, peace',
+Green: 'Nature, growth, freshness',
+Yellow: 'Hope, joy, danger',
+Orange: 'Warmth, kindness, joy',
+White: 'Truth, indifference',
+Brown: 'resilience, dependability, security, and safety. '}
+
+
+const bgc = ["yellow","green","red",'blue','purple','orange','white','brown','pink']
+
+export default () => {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+  const [meaning, setMeaning] = useState(null);
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length)
+    setPrizeNumber(newPrizeNumber)
+    setMustSpin(true)
+    setMeaning(logic[scc]);
+  }
+const sc = prizeNumber
+const scc =data[sc].option
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='App'>
+      <Wheel
+        mustStartSpinning={mustSpin}
+        prizeNumber={prizeNumber}
+        data={data}
+        backgroundColors={bgc}
 
-export default App;
+        onStopSpinning={() => {
+          setMustSpin(false);
+        }}
+      />
+      <button onClick={handleSpinClick}>SPIN</button>
+      <p className='sc'>Selected colour : {scc}</p>
+  <p>{meaning}</p>
+    </div>
+  )
+}
